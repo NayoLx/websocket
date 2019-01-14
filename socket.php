8871d3632203d8ad5ca99cacbe079990c393e794
NayoLx <690575679@qq.com>
2019-01-14 16:34:25 +0800

    build(v1.0.1) 3 更新一哈代码

--- socket.php
+++ socket.php
@@ -1,10 +1,13 @@
 <?php
-$host = '127.0.0.1'; 
-$port = '13142'; 
-$null = NULL; 
+$host = '127.0.0.1';
+$port = '13142';
+$null = NULL;
 
 //创建tcp socket
+//设置超时时间
+error_reporting(E_ALL);
 set_time_limit(0);
+ob_implicit_flush();
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
@@ -17,133 +20,139 @@ socket_listen($socket);
 $clients = array($socket);
 
 //设置一个死循环,用来监听连接 ,状态
-while (true) {
-    
-    $changed = $clients;
-    socket_select($changed, $null, $null, 0, 10);
-    
-    //如果有新的连接
-    if (in_array($socket, $changed)) {
-        //接受并加入新的socket连接
-        $socket_new = socket_accept($socket); 
-        $clients[] = $socket_new;
-        
-        //通过socket获取数据执行handshake
-        $header = socket_read($socket_new, 1024); 
-        perform_handshaking($header, $socket_new, $host, $port);
-        
+while (true){
+
+	$changed = $clients;
+	socket_select($changed, $null, $null, 0, 10);
+	//如果有新的连接
+	if (in_array($socket, $changed)) {
+
+		//接受并加入新的socket连接
+		$socket_new = socket_accept($socket);
+		$clients[] = $socket_new;
+
+		print_r($clients);
+
+		//通过socket获取数据执行handshake
+		$header = socket_read($socket_new, 1024);
+		perform_handshaking($header, $socket_new, $host, $port);
+
         //获取client ip 编码json数据,并发送通知
         socket_getpeername($socket_new, $ip);
-        $response = mask(json_encode(array('type'=>'system', 'message'=>$ip.' connected')));
+        $response = mask(json_encode(array('type' => 'system', 'message' => $ip. ' connected')));
         send_message($response);
         $found_socket = array_search($socket, $changed);
         unset($changed[$found_socket]);
-    }
-    
-    //轮询 每个client socket 连接
-    foreach ($changed as $changed_socket) {    
-        
-        //如果有client数据发送过来
-        while(socket_recv($changed_socket, $buf, 1024, 0) >= 1)
-        {
-            //解码发送过来的数据
-            $received_text = unmask($buf); 
-            $tst_msg = json_decode($received_text);  
-            $user_name = $tst_msg->name; 
-            $user_message = $tst_msg->message; 
-            $user_avatarUrl = $tst_msg->avatarUrl; 
-            
-            //把消息发送回所有连接的 client 上去
-            $response_text = mask(json_encode(array('type'=>'usermsg', 'name'=>$user_name, 'message'=>$user_message, 'avatarUrl'=>$user_avatarUrl)));
-            send_message($response_text);
-            break 2; 
-        }
-        
-        //检查offline的client
-        $buf = @socket_read($changed_socket, 1024, PHP_NORMAL_READ);
-        if ($buf === false) { 
-            $found_socket = array_search($changed_socket, $clients);
-            socket_getpeername($changed_socket, $ip);
-            unset($clients[$found_socket]);
-            $response = mask(json_encode(array('type'=>'system', 'message'=>$ip.' disconnected')));
-            send_message($response);
-        }
-    }
+
+	}
+
+	//轮询 每个client socket 连接
+	foreach ($changed as $key => $changed_socket) {
+
+		//如果有client数据发送过来
+		while (socket_recv($changed_socket, $buf, 1024, 0) >= 1) {
+			//解码发送过来的数据
+			$received_text = unmask($buf);
+			$tst_msg = json_decode($received_text);
+			if ($tst_msg) {
+			    $myuid = $key;
+				$user_name = $tst_msg -> name;
+				$user_message = $tst_msg -> message;
+				$user_avatarUrl = $tst_msg -> avatarUrl;
+
+				//把消息发送回所有连接的 client 上去
+				$response_text = mask(json_encode(array('type' => 'usermsg', 'myuid' => $myuid, 'name' => $user_name, 'message' => $user_message, 'avatarUrl' => $user_avatarUrl)));
+				if ($uid != '') {
+                    send_client_message($response_text,$myuid, $uid);
+                }
+                else {
+				    send_message($response_text);
+                }
+			}
+			break 2;
+		}
+
+		//检查offline的client
+		$buf = @socket_read($changed_socket, 1024, PHP_NORMAL_READ);
+		if ($buf === false) {
+			$found_socket = array_search($changed_socket, $clients);
+			socket_getpeername($changed_socket, $ip);
+			unset($clients[$found_socket]);
+			$response = mask(json_encode(array('type' => 'system', 'message' => $ip . ' disconnected')));
+			send_message($response);
+			break;
+		}
+	}
 }
 
+
 // 关闭监听的socket
-socket_close($sock);
+socket_close($socket);
 
 //发送消息的方法
-function send_message($msg)
-{
+function send_message($msg) {
+	global $clients;
+	foreach ($clients as $changed_socket) {
+		@socket_write($changed_socket, $msg, strlen($msg));
+	}
+	return true;
+}
+
+//指定用户发送
+function send_client_message($msg,$myuid, $uid) {
     global $clients;
-    foreach($clients as $changed_socket)
-    {
-        @socket_write($changed_socket,$msg,strlen($msg));
-    }
+    @socket_write($clients[$myuid], $msg, strlen($msg));
+    @socket_write($clients[$uid], $msg, strlen($msg));
     return true;
 }
 
-
 //解码数据
 function unmask($text) {
-    $length = ord($text[1]) & 127;
-    if($length == 126) {
-        $masks = substr($text, 4, 4);
-        $data = substr($text, 8);
-    }
-    elseif($length == 127) {
-        $masks = substr($text, 10, 4);
-        $data = substr($text, 14);
-    }
-    else {
-        $masks = substr($text, 2, 4);
-        $data = substr($text, 6);
-    }
-    $text = "";
-    for ($i = 0; $i < strlen($data); ++$i) {
-        $text .= $data[$i] ^ $masks[$i%4];
-    }
-    return $text;
+	$length = ord($text[1]) & 127;
+	if ($length == 126) {
+		$masks = substr($text, 4, 4);
+		$data = substr($text, 8);
+	} elseif ($length == 127) {
+		$masks = substr($text, 10, 4);
+		$data = substr($text, 14);
+	} else {
+		$masks = substr($text, 2, 4);
+		$data = substr($text, 6);
+	}
+	$text = "";
+	for ($i = 0; $i < strlen($data); ++$i) {
+		$text .= $data[$i] ^ $masks[$i % 4];
+	}
+	return $text;
 }
 
 //编码数据
-function mask($text)
-{
-    $b1 = 0x80 | (0x1 & 0x0f);
-    $length = strlen($text);
-    
-    if($length <= 125)
-        $header = pack('CC', $b1, $length);
-    elseif($length > 125 && $length < 65536)
-        $header = pack('CCn', $b1, 126, $length);
-    elseif($length >= 65536)
-        $header = pack('CCNN', $b1, 127, $length);
-    return $header.$text;
+function mask($text) {
+	$b1 = 0x80 | (0x1 & 0x0f);
+	$length = strlen($text);
+
+	if ($length <= 125)
+		$header = pack('CC', $b1, $length);
+	elseif ($length > 125 && $length < 65536)
+		$header = pack('CCn', $b1, 126, $length);
+	elseif ($length >= 65536)
+		$header = pack('CCNN', $b1, 127, $length);
+	return $header . $text;
 }
 
 //握手的逻辑
-function perform_handshaking($receved_header,$client_conn, $host, $port)
-{
-    $headers = array();
-    $lines = preg_split("/\r\n/", $receved_header);
-    foreach($lines as $line)
-    {
-        $line = chop($line);
-        if(preg_match('/\A(\S+): (.*)\z/', $line, $matches))
-        {
-            $headers[$matches[1]] = $matches[2];
-        }
-    }
+function perform_handshaking($receved_header, $client_conn, $host, $port) {
+	$headers = array();
+	$lines = preg_split("/\r\n/", $receved_header);
+	foreach ($lines as $line) {
+		$line = chop($line);
+		if (preg_match('/\A(\S+): (.*)\z/', $line, $matches)) {
+			$headers[$matches[1]] = $matches[2];
+		}
+	}
 
-    $secKey = $headers['Sec-WebSocket-Key'];
-    $secAccept = base64_encode(pack('H*', sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
-    $upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
-    "Upgrade: websocket\r\n" .
-    "Connection: Upgrade\r\n" .
-    "WebSocket-Origin: $host\r\n" .
-    "WebSocket-Location: ws://$host:$port/service/socket.php\r\n".
-    "Sec-WebSocket-Accept:$secAccept\r\n\r\n";
-    socket_write($client_conn,$upgrade,strlen($upgrade));
+	$secKey = $headers['Sec-WebSocket-Key'];
+	$secAccept = base64_encode(pack('H*', sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
+	$upgrade = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" . "Upgrade: websocket\r\n" . "Connection: Upgrade\r\n" . "WebSocket-Origin: $host\r\n" . "WebSocket-Location: ws://$host:$port/service/socket.php\r\n" . "Sec-WebSocket-Accept:$secAccept\r\n\r\n";
+	socket_write($client_conn, $upgrade, strlen($upgrade));
 }
